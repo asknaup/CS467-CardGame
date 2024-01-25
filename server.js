e661be0f0a0feb84d15f65846c460fc21883d810
Artem Kuryachy <akuryachy@gmail.com>
2024-01-24 18:54:23 -0700

    Week 1 routes added to rendering

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const app = express();                          // Routing
 const port = 3000;
 
 app.use(express.static('style'));               // For css
-app.engine('handlebars', exphbs.engine(         
+
+app.engine('handlebars', exphbs.engine(                  
   { extname: "hbs",
     defaultLayout: false,
     layoutsDir: "views/layouts/"
   }
 ));
+
 app.set('view engine', 'handlebars');
+
+/*
+ROUTES
+*/
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -26,6 +32,33 @@ app.get('/collection', (req, res) => {
   res.render('collection', {title: 'heelo', message: 'world 123'});
 });
 
+app.get('/current-deck-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/game-generation-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/look-at-games-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/new-user-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/reset-password-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/user-profile-page/index', (req, res) => {
+  res.render('index')
+});
+
+app.get('/welcome-page/index', (req, res) => {
+  res.render('index')
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
