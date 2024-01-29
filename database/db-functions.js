0e9275ba2a2799d3f7a7a1adb6b08c9a133f4582
jeremysteele <jeremysteelewriter@gmail.com>
2024-01-28 16:23:16 -0800

    3.3

--- database/db-functions.js
+++ database/db-functions.js
@@ -1,6 +1,6 @@
 const db = require('./db-connector');
 
-// pool.query parameters, 1st is sql query string, 2nd is optional list of params, third is (err, result) object
+// pool.query parameters; 1st is sql query string, 2nd is optional list of params, third is (err, result) object
 
 // Async Database access to avoid nested hell
 
