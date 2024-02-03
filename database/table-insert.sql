b29cf25bac2bc6f4341a58f2c356327dc6696d79
Alexander Aull <aulla@oregonstate.edu>
2024-02-03 16:16:32 -0600

    update commit-mania to main updates

---
+++ database/table-insert.sql
@@ -0,0 +1,11 @@
+-- Create dummy data
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Insert into user_creds and user_profile
+-- -----------------------------------------------------
+
+INSERT INTO user_creds (username, pwd, account_status) VALUES
+    ('aknaup', 'qwerty123456', 1),
+    ('cs467', 'cs467pwd', 1);
+
