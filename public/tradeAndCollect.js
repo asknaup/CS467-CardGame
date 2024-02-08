35667714b51ff154621591f75ac82e166e264510
Alexander Aull <aulla@oregonstate.edu>
2024-02-07 20:22:48 -0600

    used javascript to generate cards in tradeAndCollect

---
+++ public/tradeAndCollect.js
@@ -0,0 +1,33 @@
+var scrollDeck = document.getElementById("scrollDeck");
+var leftDist = 0;
+for (let i = 0; i <= 14; i++){
+    var newCard = document.createElement("div");
+    newCard.setAttribute("class", "newCard");
+    newCard.setAttribute('id', 'card' + i.toString());
+    if (i % 2 == 0){
+        let goblinImage = document.createElement('img');
+        goblinImage.src = "images/goblin-willow-tree.jpg";
+        goblinImage.style.margin = "2px";
+        newCard.appendChild(goblinImage);
+    }
+    else{
+        let darkWizardImage = document.createElement('img');
+        darkWizardImage.src = "images/dark-wizard.png";
+        darkWizardImage.style.margin = "2px";
+        newCard.appendChild(darkWizardImage);
+    }
+    newCard.style.position = "absolute";
+    newCard.style.bottom = "0vw";
+    newCard.style.left = leftDist.toString() + "vw";
+    newCard.style.height = "14.5vw";
+    newCard.style.width = "8.8vw";
+    newCard.style.backgroundColor = "beige";
+    newCard.style.borderStyle = "solid";
+    newCard.style.borderColor = "black";
+    newCard.style.borderWidth = "2px";
+    newCard.style.borderRadius = "5px";
+    newCard.style.zIndex = i.toString();
+    scrollDeck.appendChild(newCard);
+    leftDist += 6;
+}
+
