96dd6b5fa8836a7165fce5a11915f6a6e6c08155
Artem Kuryachy <akuryachy@gmail.com>
2024-02-10 14:52:09 -0700

    Larger display for cards to be staged, moved CSS from JS to CSS files

--- public/tradeAndCollect.js
+++ public/tradeAndCollect.js
@@ -12,23 +12,13 @@ function createCardElement(idString, leftVal, cardObj){
     var scrollCard = document.createElement("div");
     scrollCard.setAttribute("class", "newCard");
     scrollCard.setAttribute('id', "card" + idString);
+
     // this creates the image for the card
     let cardImage = document.createElement("img");
     cardImage.src = cardObj.imageSrc;
     cardImage.style.margin = "2px";
     scrollCard.appendChild(cardImage);
-    // this is all the css for this card
-    scrollCard.style.position = "absolute";
-    scrollCard.style.bottom = "0vw";
-    scrollCard.style.left = (leftVal).toString() + "vw";
-    scrollCard.style.height = "14.5vw";
-    scrollCard.style.width = "8.8vw";
-    scrollCard.style.backgroundColor = "beige";
-    scrollCard.style.borderStyle = "solid";
-    scrollCard.style.borderColor = "black";
-    scrollCard.style.borderWidth = "2px";
-    scrollCard.style.borderRadius = "5px";
-    scrollCard.style.zIndex = idString;
+
     // creates the same affect as #id.hover { z-index: [value goes here]}
     scrollCard.onmouseenter = function(){this.style.zIndex = "999"};
     scrollCard.onmouseleave = function(){this.style.zIndex = idString};
@@ -56,7 +46,7 @@ function displayScrollCards(startIndex, endIndex, cardArr){
 var cardArr = [];
 for (let i = 0; i<=27; i++){
     var newCard = null;
-    if(i <= 14){
+    if(i <= 6){
         if (i % 2 == 0){
             newCard = new Card("card" + i.toString(), "images/goblin-willow-tree.jpg");
         }
@@ -71,11 +61,11 @@ for (let i = 0; i<=27; i++){
 
 /* main code for tradeAndCollect */
 var startIndex = 0;
-var endIndex = 14;
+var endIndex = 6;
 displayScrollCards(startIndex, endIndex, cardArr);
 var scrollRightButton = document.getElementById("scrollRight");
 scrollRightButton.addEventListener("click", () => {
-    if (cardArr.length - endIndex < 14){
+    if (cardArr.length - endIndex < 6){
         endIndex = cardArr.length
         startIndex = startIndex + (cardArr.length - endIndex)
     }else{
