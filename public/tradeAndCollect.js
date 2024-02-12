04ceb840ae7f22376656a771232797502d271a6b
Alexander Aull <aulla@oregonstate.edu>
2024-02-12 14:24:20 -0600

    added new functionality to tradeAndCollect

--- public/tradeAndCollect.js
+++ public/tradeAndCollect.js
@@ -6,6 +6,7 @@ class Card{
         this.description = description;
         this.additionalText = additionalText;
         this.attributes = attributes;
+        this.isStaged = false;
     }
 }
 
@@ -52,12 +53,23 @@ function appendDescription(cardObj, scrollCard){
     scrollCard.appendChild(description);
 }
 
+function addHoverFunctionality(width, height, card){
+    card.onmouseenter = function(){
+        card.style.width = (width + 1).toString() + "vw";;
+        card.style.height = (height + 1).toString() + "vw";
+    };
+    card.onmouseleave = function(){
+        card.style.width = (width).toString() + "vw";
+        card.style.height = (height).toString() + "vw";
+    };
+}
+
 // TODO: Standardize card form format
-function createCardElement(position, width, height, cardObj){
+function createCardElement(position, width, height, id, cardArrIndex, cardArr){
+    var cardObj = cardArr[cardArrIndex];
     // create the div that becomes the card
     var scrollCard = document.createElement("div");
-    scrollCard.setAttribute("class", "newCard");
-    scrollCard.setAttribute('id', cardObj.primaryKey);
+    scrollCard.setAttribute('id', id);
     appendTitle(cardObj, scrollCard);
     appendImage(cardObj, scrollCard);
     // this is all the css for this card
@@ -68,33 +80,53 @@ function createCardElement(position, width, height, cardObj){
     scrollCard.style.height = (height).toString() + "vw";
     scrollCard.style.color = "black";
     scrollCard.style.backgroundColor = "beige";
-    scrollCard.style.border = "3px solid black";
+    if (cardObj.isStaged == false){
+        scrollCard.style.border = "3px solid black";
+    }else{
+        scrollCard.style.border = "6px solid #4caf50";
+    }
     scrollCard.style.borderRadius = "5px";
     appendAttributes(cardObj, scrollCard);
     appendDescription(cardObj, scrollCard);
-    // creates the same affect as #id.hover { z-index: [value goes here]}
-    scrollCard.onmouseenter = function(){
-        this.style.width = (width + 1).toString() + "vw";;
-        this.style.height = (height + 1).toString() + "vw";
-    };
-    scrollCard.onmouseleave = function(){
-        scrollCard.style.width = (width).toString() + "vw";
-        scrollCard.style.height = (height).toString() + "vw";
-    };
-    scrollCard.onclick = function(){
-        if(this.parentNode.id === "cardSlots"){
-            this.removeAttribute("class");
-            var stageAreaZero = document.getElementById("stageAreaZero");
-            stageAreaZero.appendChild(this);
-        };
-    };
+    addHoverFunctionality(width, height, scrollCard);
     return scrollCard;
 }
 
-function setCardPlacement(left, bottom, scrollCard){
-    scrollCard.style.left = (left).toString() + "vw";
-    scrollCard.style.bottom = (bottom).toString() + "vw";
+function addStagedCardFunctionality(cardArrIndex, cardArr){
+    var startIndex = Math.floor(cardArrIndex / numScrollCards) * numScrollCards;
+    var endIndex = startIndex + (numScrollCards - 1);
+    if (endIndex > cardArr.length - 1){
+        endIndex = cardArr.length - 1
+    }
+    displayScrollCards(startIndex, endIndex, cardArr);
 }
+
+function addScrollCardFunctionality(width, height, cardArrIndex, cardArr, scrollCard){
+    scrollCard.onclick = function(){
+        if (cardArr[cardArrIndex].isStaged == false){
+            if(stagedCardCount < 8){
+                scrollCard.style.border = "6px solid #4caf50";
+                var stageArea = document.getElementById("stageAreaZero");
+                var newId = scrollCard.id + "StagedCard";
+                let stagedCard = createCardElement("relative", width, height, newId, cardArrIndex, cardArr);
+                stagedCard.onclick = function(){addStagedCardFunctionality(cardArrIndex, cardArr)};
+                stageArea.appendChild(stagedCard);
+                cardArr[cardArrIndex].isStaged = true;
+                //stagedCardCount is a global variable
+                stagedCardCount += 1;
+                console.log("if runs");
+            }
+        } else {
+            scrollCard.style.border = "3px solid black";
+            var stagedCard = document.getElementById(scrollCard.id + "StagedCard");
+            stagedCard.remove();
+            cardArr[cardArrIndex].isStaged = false;
+            //stagedCardCount is a global variable
+            stagedCardCount -= 1;
+        }
+    };
+}
+
 
 function displayScrollCards(startIndex, endIndex, cardArr){
     // Clear out old card elements
@@ -105,8 +137,11 @@ function displayScrollCards(startIndex, endIndex, cardArr){
     var cardSlots = document.getElementById("cardSlots");
     var width = 10.5;
     var height = 18.5;
-    for (let i = startIndex; i <= endIndex; i++){
-        scrollCard = createCardElement("relative", width, height, cardArr[i]);
+    for (let index = startIndex; index <= endIndex; index++){
+        var elementId = cardArr[index].primaryKey;
+        scrollCard = createCardElement("relative", width, height, elementId, index, cardArr);
+        scrollCard.setAttribute("class", "newCard");
+        addScrollCardFunctionality(width, height, index, cardArr, scrollCard);
         cardSlots.appendChild(scrollCard);
     }
 }
@@ -114,13 +149,13 @@ function displayScrollCards(startIndex, endIndex, cardArr){
 /* Create Example Dummy Cards */
 // Example cards data (you can add more)
 var exampleCards = [
-    goblinObj = {primaryKey: "primaryKey0", cardName: "Goblin", imageSrc: 'images/goblin-willow-tree.jpg', 
+    goblinObj = {primaryKey: "goblin", cardName: "Goblin", imageSrc: 'images/goblin-willow-tree.jpg', 
             description: 'A small forest goblin.', additionalText:'Creature', 
             attributes: {hp:50, atk:50, def:30, specialAbility:'Cooking', goldCost:5}},
-    wizardObj = {primaryKey: "primaryKey1", cardName: 'Fire Ball Scroll', imageSrc: 'images/dark-wizard.png',  
+    wizardObj = {primaryKey: "wizard", cardName: 'Fire Ball Scroll', imageSrc: 'images/dark-wizard.png',  
                 description:'A powerful fire ball.', additionalText: 'Spell', 
                 attributes: {hp:80, atk:60, def:20, specialAbility:'Fire Ball', goldCost:7}},
-    iceDragonObj = {primaryKey: "primaryKey2", cardName: 'Ice Dragon', imageSrc: 'images/ice-dragon.png', 
+    iceDragonObj = {primaryKey: "iceDragon", cardName: 'Ice Dragon', imageSrc: 'images/ice-dragon.png', 
                     description: 'An ice dragon from the North.', additionalText: 'Creature', 
                     attributes: {hp:120, atk:100, def:80, specialAbility:'Flying', goldCost:9}}
     // Add more cards as needed
@@ -131,12 +166,13 @@ for (let index = 0; index<=33; index++){
     let numCards = exampleCards.length;
     let randomIndex = Math.floor(Math.random() * numCards);
     let cardObj = exampleCards[randomIndex];
-    cardArr.push(new Card(cardObj.primaryKey, cardObj.cardName, cardObj.imageSrc,
+    cardArr.push(new Card(cardObj.primaryKey + index.toString(), cardObj.cardName, cardObj.imageSrc,
                 cardObj.description, cardObj.additionalText, cardObj.attributes));
 }
 
 
 /* main code for tradeAndCollect */
+var stagedCardCount = 0;
 var numScrollCards = 8;
 var startIndex = 0;
 var endIndex = numScrollCards - 1;
