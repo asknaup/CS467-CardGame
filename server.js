f8494d3503c071f35a7cd5dfc7c16ed12c820b2e
jeremysteele <jeremysteelewriter@gmail.com>
2024-02-27 11:59:38 -0800

    edits

--- server.js
+++ server.js
@@ -61,11 +61,6 @@ app.use('/game', express.static(path.join(__dirname, 'game')));
 /*
 ROUTES
 */
-// To DO
-// Generate Game Instances with Name
-// Generate Collection Instances of Games per User
-// Generate Decks of collection 
-
 
 app.get('/', (req, res) => {
   const user = req.session.user
@@ -143,7 +138,6 @@ app.get('/gamePlayPage', (req, res) => {
   }
 });
 
-
 app.get('/cardGenBulkPage', async (req, res) => {
   const user = req.session.user;
   if (user) {
@@ -154,10 +148,6 @@ app.get('/cardGenBulkPage', async (req, res) => {
   }
 });
 
-// TODO routing between gameGeneration, card Generation
-// TODO Work on corresponding edit pages, corresponding bulk pages
-// TODO Work on generating inputs
-// TODO html formatting
 app.get('/gameGenPage', async (req, res) => {
   const user = req.session.user;
   try {
@@ -187,12 +177,10 @@ app.get('/generatedGameView', async (req, res) => {
 });
 
 
-// TODO: One of these to be deleted
 app.get('/buildDeck', async (req, res) => {
   // Needs collection and game info
   // const user = req.session.user;
   const user = {userId: 1001, username: 'admin'};
-
   // FIXME: Switch back to const user = req.session.user; once buildDeck complete
   try {
     if (user) {
@@ -207,26 +195,12 @@ app.get('/buildDeck', async (req, res) => {
   }
 });
 
-// TODO Deck generation page
-app.get('/buildDeck', (req, res) => {
-  // Show user logged in user profile
-  // FIXME
-  const user = req.session.user;
-  // const user = { userId: 1001, username: 'admin' }
-  if (user) {
-    res.render('buildDeck', { showLogoutButton: true, userId: 1001 })
-  } else {
-    res.render('buildDeck', { showLogoutButton: false })
-  }
-});
-
 // Add to deck, delete, deck stats
 app.get('/currentDeck', async (req, res) => {
   // Show user logged in user profile
   // FIXME
   // const user = { userId: 1001, username: 'admin' };
   const user = req.session.user;
-
   try {
     const receivedData = req.body;
     console.log(receivedData);
@@ -244,28 +218,20 @@ app.get('/deckNames', async (req, res) => {
 });
 
 app.post('/deckCards', async (req, res) => {
-
   const selectedDeck = req.body.deckId;
-
   // Store the selected deckId in the session
   req.session.deck = { deckId: parseInt(selectedDeck) };
-
   console.log("selectedDeck post", selectedDeck);
   console.log("session after post", req.session);
-
   res.json({ status: 'OK' }); // Send a JSON response
 });
 
 app.get('/getCardsForDeck', async (req, res) => {
   try {
-
     const selectedDeck = req.query.deckId;
-
     if (selectedDeck) {
-
       // Fetch cardIds for the selected deck and current user from the database using a Promise
       const row = await dbFunc.getUserDeck(parseInt(selectedDeck));
-
       if (row && row.length > 0) {
         var deckObject = JSON.parse(row[0].cardId);
         res.json(deckObject.cardList);
@@ -274,10 +240,8 @@ app.get('/getCardsForDeck', async (req, res) => {
         return;
       }
     } else {
-      console.log("dfjaldfj");
       res.status(400).send("Missing deckId parameter");
       return;
-
     }
   } catch (error) {
     console.error('Error handling getCardsForDeck:', error);
@@ -325,7 +289,6 @@ app.get('/resetPW', (req, res) => {
   }
 });
 
-// Will need minor work
 app.get('/cardGenPage', async (req, res) => {
   const user = req.session.user;
   if (user) {
@@ -482,10 +445,8 @@ app.post('/cardViewEditPage', async (req, res) => {
     if (user) {
       async function delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
       const generatedCards = [];
-      console.log(req.body.whichgame);
       const collectionId = await dbFunc.insertOrSelectCollectionByUserIdandGameId(user.userId, req.body.whichgame)
       req.session.collectionId = collectionId;
-      console.log(collectionId, user.userId, req.body.whichgame);
       req.session.gameId = req.body.whichgame;
 
       if (req.body.cardType == "Creature") {
@@ -548,9 +509,11 @@ app.post('/cardViewEditPage', async (req, res) => {
 
 app.post('/cardViewPrintedPage', async (req, res) => {
   const user = req.session.user;
+  let cardIdList = [];
   try {
     const stringCard = JSON.parse(req.body.cardstring);
     const cardId = await dbFunc.insertCard(stringCard.name, stringCard.cardType, user.userId, stringCard.rarity, stringCard.manaCost);
+    cardIdList.push(cardId);
     await dbFunc.insertCardUrl(cardId, stringCard.URL);
 
     if (req.body.cardType == "Creature") {
@@ -559,28 +522,16 @@ app.post('/cardViewPrintedPage', async (req, res) => {
       await dbFunc.insertSpellCard(cardId, stringCard.spellType, stringCard.ability, stringCard.attack, stringCard.defense, stringCard.utility); // Needs work
     }
 
-    // Needs Work
-    '{"cardList": []}'
-
-    /// cardId: '{"cardList": []}'
-    /*
+    // Update User Collection
     try {
-      // Fetch the list of card IDs from the collection
-      let returnList = await dbFunc.grabListOfCardsFromCollection(req.session.collectionId);
-      x = JSON.parse(returnList.cardIds);
-      console.log(returnList);
-      x.push(cardId);
-      console.log(returnList);
-      
-      // const updatedListString = JSON.stringify(cardIds);
-      await dbFunc.updateListOfCollection(req.session.collectionId, JSON.stringify({"cardList": x}));
-      console.log("Updated collection with new card ID:", cardId);
+      const collId = await dbFunc.insertOrSelectCollectionByUserIdandGameId(user.userId, req.body.whichgame);
+      let returnList = await dbFunc.grabListOfCardsFromCollection(collId);
+      x = JSON.parse(returnList[0].cardId);
+      y = x.cardList.concat(cardIdList);
+      await dbFunc.updateListOfCollection(collId, JSON.stringify({"cardList": y}));
      } catch (error) {
-      // Error occurred while updating the collection
-      console.error("Error updating collection:", error);
+      console.error("Error updating collection:", error); 
     }
-    */
-
     const data = await dbFunc.getCardInfo(cardId);
     res.render('cardViewPrintedPage', {
       card: stringCard,
@@ -626,7 +577,6 @@ app.post('/cardViewPrintedBulkPage', async (req, res) => {
         generatedCards.push(newSpell);
     }   
   };
-    // console.log(generatedCards);
     generatedCards.forEach( async (card) => {
       const cardId = await dbFunc.insertCard(card.name, card.cardType, user.userId, card.rarity, card.manaCost);
       cardIdList.push(cardId);
@@ -637,29 +587,23 @@ app.post('/cardViewPrintedBulkPage', async (req, res) => {
         await dbFunc.insertCreatureCard(cardId, card.attack, card.defense, card.creature); 
       }
     });
-      
+      // Update User Collection
     try {
       const collId = await dbFunc.insertOrSelectCollectionByUserIdandGameId(user.userId, req.body.whichgame);
-      console.log(collId);
       let returnList = await dbFunc.grabListOfCardsFromCollection(collId);
-      console.log(returnList);
       x = JSON.parse(returnList[0].cardId);
-      console.log(cardIdList);
       y = x.cardList.concat(cardIdList);
-      console.log(y);
       await dbFunc.updateListOfCollection(collId, JSON.stringify({"cardList": y}));
      } catch (error) {
       console.error("Error updating collection:", error); 
     }
     res.render('cardViewPrintedBulkPage', { cards: generatedCards });
-
   } catch (err) {
     // Handle errors
     console.error(err);
     res.status(500).send({ success: false, error: err.message });
   }
 });
-
 
 app.post('/generatedGameView', async (req, res) => {
   if (req.session.user) {
