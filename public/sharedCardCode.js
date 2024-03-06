5c30254fd41e9f1e46c6c809639158ffc6f9d489
jeremysteele <jeremysteelewriter@gmail.com>
2024-03-06 14:52:01 -0800

    edits to small places

--- public/sharedCardCode.js
+++ public/sharedCardCode.js
@@ -17,7 +17,7 @@ function createTradingCard(cardData) {
     // toolTipText.classList.add('toolTipText');
 
     // Name
-    var cardName = document.createElement('h1');
+    var cardName = document.createElement('h1');                                                                                                                                                                                                                                                                        
     cardName.classList.add('cardName');
     cardName.textContent = cardData.cardName;
 
